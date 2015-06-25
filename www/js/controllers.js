d79f7ff5b67c32bf126937d3297ad7ac97e9ed87
Noah Hines <hinesnoa@msu.edu>
2015-06-24 20:36:30 -0500

    Oh boy I think it works.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,32 +10,37 @@ angular.module('starter.controllers', [])
 
 // Map Controller
 .controller('MapCtrl', function($scope) {
-	var userMarkerShown = false;
-	// Create a map in the #map div
-	var map = L.map('map',
-		{ zoomControl:false } // Zoom controls not needed for mobile
-		);
+	// MAPS
 
-	// Locate library used to get user's location
-	map.locate({setView: true, watch: true}) /* This will return map so you can do chaining */
-	.on('locationfound', function(e){
-		if (userMarkerShown == true) {
-			map.removeLayer(userMarker);
-			userMarkerShown = false;
+	angular.extend($scope, {
+		center: {
+			autoDiscover: true
 		}
+	});
 
-		var userMarker = L.marker([e.latitude, e.longitude]).bindPopup('You are <b>alive</b>!');
 
-		map.addLayer(userMarker);
-		userMarkerShown = true;
+	var mainMarker = {
+		lat: 51,
+		lng: 0,
+		focus: true,
+		message: "Hey, drag me if you want",
+		draggable: true
+	};
+	angular.extend($scope, {
+
+		markers: {
+			mainMarker: angular.copy(mainMarker)
+		},
+		position: {
+			lat: 51,
+			lng: 0
+		}
+	});
+
+
+}, 'centerService', function ($scope, CenterService) {
+	angular.extend($scope, {
+		center: CenterService.center,
+		bounds: {},
 	})
-	.on('locationerror', function(e){
-		console.log("Location access denied.");
-		console.log(e);
-	});
-
-	  // add OpenStreetMap tile layer
-	  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	  	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	  }).addTo(map);
-	});
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,44 @@
-angular.module('starter.services', []);
+angular.module('starter.services', [])
+
+
+.service('CenterService', [
+    '$location',
+    'localStorageService',
+    function CenterService ($location, localStorageService) {
+        var Service = {
+            center: {}
+        };
+        var locationParameters = $location.search();
+
+        // Get center from url if available ...
+        if(locationParameters.initLng !== undefined && !isNaN(parseFloat(locationParameters.initLng))
+            && locationParameters.initLat !== undefined && !isNaN(parseFloat(locationParameters.initLat))
+            && locationParameters.initZoom !== undefined && !isNaN(parseInt(locationParameters.initZoom))) {
+            Service.center =  {
+                lat: parseFloat(locationParameters.initLat),
+                lng: parseFloat(locationParameters.initLng),
+                zoom: parseInt(locationParameters.initZoom),
+                autoDiscover: false,
+            }
+        } else {
+            if(localStorageService.keys().indexOf('center') > -1) {
+                // ... or from localStorage ...
+                Service.center = localStorageService.get('center');
+            } else {
+                // ... or else from the configuration
+                Service.center = MapConfig.map.center;  
+            }
+        }
+
+        Service.save = function(center){
+            center.lat = parseFloat(center.lat);
+            center.lng = parseFloat(center.lng);
+            localStorageService.set('center', center);
+            angular.extend(Service.center, center);
+            console.log(center);
+        }
+
+        return Service
+    }
+    ]);
+
