8a0f564e630e84680f527282326e06951d2924bf
Noah Hines <hinesnoa@msu.edu>
2015-06-28 17:16:34 -0500

    Removed leaflet directive and added the range directive for the race timeline.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,55 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function($scope, MyTimer) {
+	var userSpeed = 1;
+	var chickenSpeed = 0.6;
+
+	$scope.t = MyTimer(1000);
+
+
+	// Launch plugin
+	$("#example").ionRangeSlider({
+	    type: "double",
+	    min: 0,
+	    max: 1000,
+	    from: 200,
+	    to: 500,
+	    grid: true
+	});
+
+	// Saving it's instance to var
+	var slider = $("#example").data("ionRangeSlider");
+
+
+
+	$scope.setPosition = function() {
+		// return ($scope.t.totalMilliseconds * userSpeed)/100;
+		slider.update({
+		    from: ($scope.t.totalMilliseconds * chickenSpeed)/100 - 20,
+		    to: ($scope.t.totalMilliseconds * userSpeed)/100
+		});
+	};
+
+	$scope.getMaxDistance = function() {
+		return 100;
+	};
+
+	$scope.getMinDistance = function() {
+		return 0;
+	};
+
+
+
+
+
+})
 
 .controller('LiveCtrl', function($scope) {})
 
 .controller('LeaderboardCtrl', function($scope) {})
 
 
-
 // Map Controller
 .controller('MapCtrl', function($scope) {
-	// MAPS
 
-	angular.extend($scope, {
-		center: {
-			autoDiscover: true
-		}
-	});
-
-
-	var mainMarker = {
-		lat: 51,
-		lng: 0,
-		focus: true,
-		message: "Hey, drag me if you want",
-		draggable: true
-	};
-	angular.extend($scope, {
-
-		markers: {
-			mainMarker: angular.copy(mainMarker)
-		},
-		position: {
-			lat: 51,
-			lng: 0
-		}
-	});
-
-
-}, 'centerService', function ($scope, CenterService) {
-	angular.extend($scope, {
-		center: CenterService.center,
-		bounds: {},
-	})
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,44 +1,14 @@
 angular.module('starter.services', [])
 
-
-.service('CenterService', [
-    '$location',
-    'localStorageService',
-    function CenterService ($location, localStorageService) {
-        var Service = {
-            center: {}
-        };
-        var locationParameters = $location.search();
-
-        // Get center from url if available ...
-        if(locationParameters.initLng !== undefined && !isNaN(parseFloat(locationParameters.initLng))
-            && locationParameters.initLat !== undefined && !isNaN(parseFloat(locationParameters.initLat))
-            && locationParameters.initZoom !== undefined && !isNaN(parseInt(locationParameters.initZoom))) {
-            Service.center =  {
-                lat: parseFloat(locationParameters.initLat),
-                lng: parseFloat(locationParameters.initLng),
-                zoom: parseInt(locationParameters.initZoom),
-                autoDiscover: false,
-            }
-        } else {
-            if(localStorageService.keys().indexOf('center') > -1) {
-                // ... or from localStorage ...
-                Service.center = localStorageService.get('center');
-            } else {
-                // ... or else from the configuration
-                Service.center = MapConfig.map.center;  
-            }
-        }
-
-        Service.save = function(center){
-            center.lat = parseFloat(center.lat);
-            center.lng = parseFloat(center.lng);
-            localStorageService.set('center', center);
-            angular.extend(Service.center, center);
-            console.log(center);
-        }
-
-        return Service
-    }
-    ]);
+.factory('MyTimer', function($interval){
+    return function(delay){
+        var initialMs= (new Date()).getTime();
+        var result = {totalMilliseconds:0, counts:0};                
+        $interval(function() {
+            result.totalMilliseconds = (new Date()).getTime() - initialMs;
+            result.counts++;
+        }, delay);
+        return result;
+    };
+ });
 
